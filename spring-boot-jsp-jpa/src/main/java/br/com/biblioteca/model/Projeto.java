ce21105258919f8cd77d31e2feacce3bb152d052
DaltonSalvatti <dalton.salvatti@dasal.net.br>
2021-11-11 12:56:22 -0300

    Construir projeto

--- spring-boot-jsp-jpa/src/main/java/br/com/biblioteca/model/Projeto.java
+++ spring-boot-jsp-jpa/src/main/java/br/com/biblioteca/model/Projeto.java
@@ -45,6 +45,94 @@ public class Projeto {
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="idgerente")
 	private Pessoa gerente;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Set<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(Set<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData_inicio() {
+		return data_inicio;
+	}
+
+	public void setData_inicio(Date data_inicio) {
+		this.data_inicio = data_inicio;
+	}
+
+	public Date getData_previsao_fim() {
+		return data_previsao_fim;
+	}
+
+	public void setData_previsao_fim(Date data_previsao_fim) {
+		this.data_previsao_fim = data_previsao_fim;
+	}
+
+	public Date getData_fim() {
+		return data_fim;
+	}
+
+	public void setData_fim(Date data_fim) {
+		this.data_fim = data_fim;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Float getOrcamento() {
+		return orcamento;
+	}
+
+	public void setOrcamento(Float orcamento) {
+		this.orcamento = orcamento;
+	}
+
+	public String getRisco() {
+		return risco;
+	}
+
+	public void setRisco(String risco) {
+		this.risco = risco;
+	}
+
+	public Pessoa getGerente() {
+		return gerente;
+	}
+
+	public void setGerente(Pessoa gerente) {
+		this.gerente = gerente;
+	}
 	
 	
 }
